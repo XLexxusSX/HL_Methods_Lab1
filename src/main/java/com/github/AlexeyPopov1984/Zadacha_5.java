fee69762b59f1e8d6029e5e6bf8d0f2ffd886b94
Alexey.Popov <lexxuss2008@gmail.com>
2025-11-15 12:09:05 +0700

    Все 5 заданий лабораторно работы 1 выполнены.

--- src/main/java/com/github/AlexeyPopov1984/Zadacha_5.java
+++ src/main/java/com/github/AlexeyPopov1984/Zadacha_5.java
@@ -6,11 +6,35 @@ public class Zadacha_5 {
     public static void zadacha_5() {
 
         System.out.println("\nRESHENIE ZADACHI 5:");
+        System.out.print("1) v dannom otvete strochnye i propisnye bukvy schitayutsya odinakovymi;\n2) probely ne schitayutsya simvolom.\n");
         Scanner scanner = new Scanner(System.in);
-        String strForCheck = scanner.nextLine();
-
+        System.out.print("\nVvedite stroku: ");
+        String strToCheck = scanner.nextLine();
+        System.out.println("\n" + checkPaliStr(strToCheck));
     }
 
-    private static void checkPolindrom(String _strForCheck) {
+    private static String checkPaliStr(String _strToCheck) {
+
+        String answer = "";
+        String lowerCaseStrWithoutSpaces = _strToCheck.replace(" ", "").toLowerCase();
+        Boolean palindrom = true;
+
+        for (int i = 0; i < lowerCaseStrWithoutSpaces.length() / 2; i++) {
+            if (lowerCaseStrWithoutSpaces.charAt(i) != lowerCaseStrWithoutSpaces.charAt(lowerCaseStrWithoutSpaces.length() - 1 - i)) {
+                answer = "Bez probelov i v nizhnem registre: " + lowerCaseStrWithoutSpaces + "\nVvedennaya stroka NE palindrom!";
+                palindrom = false;
+                break;
+            }
+
+            if (palindrom) {
+                answer = "Bez probelov i v nizhnem registre: " + lowerCaseStrWithoutSpaces + "\nVvedennaya stroka PALINDROM!";
+            }
+        }
+
+        if (lowerCaseStrWithoutSpaces.length() == 1) {
+            answer = "Stroka sostoit iz odnogo simvola!";
+        }
+
+        return answer;
     }
 }
